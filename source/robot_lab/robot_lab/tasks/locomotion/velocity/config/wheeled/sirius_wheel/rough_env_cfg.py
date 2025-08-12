3d992822df889bd1777ba37a3781b7e9a2a046df
唐天扬 <ttyaigsw@gmail.com>
2025-08-12 11:04:55 +0800

    X

--- source/robot_lab/robot_lab/tasks/locomotion/velocity/config/wheeled/sirius_wheel/rough_env_cfg.py
+++ source/robot_lab/robot_lab/tasks/locomotion/velocity/config/wheeled/sirius_wheel/rough_env_cfg.py
@@ -231,7 +231,7 @@ class CUHKLRLSiriusWRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
             self.disable_zero_weight_rewards()
 
         # ------------------------------Terminations------------------------------
-        # self.terminations.illegal_contact.params["sensor_cfg"].body_names = [self.base_link_name, ".*_hip"]
+        self.terminations.illegal_contact.params["sensor_cfg"].body_names = [self.base_link_name, ".*_hip"]
 
         # ------------------------------Commands------------------------------
         self.commands.base_velocity.ranges.lin_vel_x = (-1.5, 1.5)
