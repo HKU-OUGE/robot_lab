86ef9d472fe0c579d48ebe41efa0a340ee712a1b
唐天扬 <ttyaigsw@gmail.com>
2025-08-19 11:35:46 +0800

    feat: Add command curriculums

--- source/robot_lab/robot_lab/tasks/locomotion/velocity/mdp/__init__.py
+++ source/robot_lab/robot_lab/tasks/locomotion/velocity/mdp/__init__.py
@@ -16,3 +16,4 @@ from .events import *  # noqa: F401, F403
 from .observations import *  # noqa: F401, F403
 from .rewards import *  # noqa: F401, F403
 from .backflip import *  # noqa: F401, F403
+from .curriculums import *  # noqa: F401, F403
