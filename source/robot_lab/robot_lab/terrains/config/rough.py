161cff8d4b00404a011be1b95eb446cadfb02006
唐天扬 <ttyaigsw@gmail.com>
2025-08-23 15:21:07 +0800

    fix: Update env to slower the wheel

--- source/robot_lab/robot_lab/terrains/config/rough.py
+++ source/robot_lab/robot_lab/terrains/config/rough.py
@@ -103,7 +103,7 @@ STAIR_TERRAINS_CFG = TerrainGeneratorCfg(
     sub_terrains={
         "pyramid_stairs": terrain_gen.MeshPyramidStairsTerrainCfg(
             proportion=0.3,
-            step_height_range=(0.10, 0.55),
+            step_height_range=(0.10, 1.0),
             step_width=1.5,
             platform_width=3.0,
             border_width=1.0,
@@ -111,17 +111,17 @@ STAIR_TERRAINS_CFG = TerrainGeneratorCfg(
         ),
         "pyramid_stairs_inv": terrain_gen.MeshInvertedPyramidStairsTerrainCfg(
             proportion=0.3,
-            step_height_range=(0.10, 0.55),
+            step_height_range=(0.10, 1.0),
             step_width=1.5,
             platform_width=3.0,
             border_width=1.0,
             holes=False,
         ),
         "hf_pyramid_slope": terrain_gen.HfPyramidSlopedTerrainCfg(
-            proportion=0.10, slope_range=(0.0, 0.4), platform_width=2.0, border_width=0.25
+            proportion=0.10, slope_range=(0.0, 0.5), platform_width=2.0, border_width=0.25
         ),
         "hf_pyramid_slope_inv": terrain_gen.HfInvertedPyramidSlopedTerrainCfg(
-            proportion=0.10, slope_range=(0.0, 0.4), platform_width=2.0, border_width=0.25
+            proportion=0.10, slope_range=(0.0, 0.5), platform_width=2.0, border_width=0.25
         ),
     },
 )
@@ -156,7 +156,7 @@ FLOATING_RING_TERRAINS_CFG = TerrainGeneratorCfg(
         "floating_ring": terrain_gen.trimesh.mesh_terrains_cfg.MeshFloatingRingTerrainCfg(
             proportion=1.0,                           # 完全生成此地形
             ring_width_range=(2.5, 2.5),              # 环的宽度范围（中心向外延伸 0.5~1.0 米）
-            ring_height_range=(0.15, 0.15),             # 环的离地高度范围
+            ring_height_range=(0.1, 0.35),             # 环的离地高度范围
             ring_thickness=0.1,                       # 环厚度（z 方向）
             platform_width=5.0,                       # 地形中心的方形平台大小
             size=(10.0, 10.0),                        # 每块地形大小
@@ -197,7 +197,7 @@ PIT_TERRAINS_CFG = TerrainGeneratorCfg(
     sub_terrains={
         "pit": terrain_gen.mesh_terrains_cfg.MeshPitTerrainCfg(
             proportion=1.0,                     
-            pit_depth_range=(0.30, 0.5),              # 坑的深度范围
+            pit_depth_range=(0.35, 0.55),              # 坑的深度范围
             platform_width=3.0,                      # 中心平台宽度
             double_pit=True,                         # 启用双层坑（更难）
             size=(10.0, 10.0),                       # 每块子地形的大小
