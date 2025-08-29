555ec300df26e835da09d94688b3fa8d0f546745
TANG Tianyang <ttyaigsw@gmail.com>
2025-08-29 20:14:22 +0800

    test: DIsable wheel mirror

--- source/robot_lab/robot_lab/tasks/locomotion/velocity/config/wheeled/sirius_wheel/rough_env_cfg.py
+++ source/robot_lab/robot_lab/tasks/locomotion/velocity/config/wheeled/sirius_wheel/rough_env_cfg.py
@@ -72,7 +72,7 @@ class CUHKLRLSiriusWRewardsCfg(RewardsCfg):
     )
     wheel_mirror = RewTerm(
         func=mdp.wheel_mirror,
-        weight=-0.001,
+        weight=0.0,
         params={
             "asset_cfg": SceneEntityCfg("robot"),
             "mirror_joints": [["LF_WHEEL", "RF_WHEEL"], ["LH_WHEEL", "RH_WHEEL"]],
