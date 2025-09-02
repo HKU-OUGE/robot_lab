7f67d94120f12732c3f3ea1ed86f0658857864d1
唐天扬 <ttyaigsw@gmail.com>
2025-09-02 18:02:38 +0800

    test: Update env

--- source/robot_lab/robot_lab/terrains/config/rough.py
+++ source/robot_lab/robot_lab/terrains/config/rough.py
@@ -192,9 +192,9 @@ FLOATING_RING_TERRAINS_CFG = TerrainGeneratorCfg(
     sub_terrains={
         "floating_ring": terrain_gen.trimesh.mesh_terrains_cfg.MeshFloatingRingTerrainCfg(
             proportion=0.4,                           # 完全生成此地形
-            ring_width_range=(3.0, 3.0),              # 环的宽度范围（中心向外延伸 0.5~1.0 米）
-            ring_height_range=(0.0, 0.3),             # 环的离地高度范围
-            ring_thickness=0.1,                       # 环厚度（z 方向）
+            ring_width_range=(2.0, 2.0),              # 环的宽度范围（中心向外延伸 0.5~1.0 米）
+            ring_height_range=(0.1, 0.5),             # 环的离地高度范围
+            ring_thickness=0.01,                       # 环厚度（z 方向）
             platform_width=3.0,                       # 地形中心的方形平台大小
             size=(5.0, 5.0),                        # 每块地形大小
             flat_patch_sampling={
@@ -207,7 +207,7 @@ FLOATING_RING_TERRAINS_CFG = TerrainGeneratorCfg(
         ),
         "pyramid_stairs": terrain_gen.MeshPyramidStairsTerrainCfg(
             proportion=0.2,
-            step_height_range=(0.10, 0.3),
+            step_height_range=(0.10, 0.5),
             step_width=1.5,
             platform_width=3.0,
             border_width=1.0,
@@ -221,7 +221,7 @@ FLOATING_RING_TERRAINS_CFG = TerrainGeneratorCfg(
             },
         ),
         "hf_pyramid_slope": terrain_gen.HfPyramidSlopedTerrainCfg(
-            proportion=0.10, slope_range=(0.0, 0.3), platform_width=2.0, border_width=0.25,
+            proportion=0.10, slope_range=(0.0, 0.4), platform_width=2.0, border_width=0.25,
             flat_patch_sampling={
                 "target": FlatPatchSamplingCfg(
                     num_patches=50,   
@@ -231,7 +231,7 @@ FLOATING_RING_TERRAINS_CFG = TerrainGeneratorCfg(
             },
         ),
         "hf_pyramid_slope_inv": terrain_gen.HfInvertedPyramidSlopedTerrainCfg(
-            proportion=0.10, slope_range=(0.0, 0.3), platform_width=2.0, border_width=0.25,
+            proportion=0.10, slope_range=(0.0, 0.4), platform_width=2.0, border_width=0.25,
             flat_patch_sampling={
                 "target": FlatPatchSamplingCfg(
                     num_patches=50,    
