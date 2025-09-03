418c3e4198b4c52acd765f77b84b83ec7ba046a2
TANG Tianyang <ttyaigsw@gmail.com>
2025-09-04 01:10:02 +0800

    test: Blind 40cm succeed

--- source/robot_lab/robot_lab/terrains/config/rough.py
+++ source/robot_lab/robot_lab/terrains/config/rough.py
@@ -193,8 +193,8 @@ FLOATING_RING_TERRAINS_CFG = TerrainGeneratorCfg(
         "floating_ring": terrain_gen.trimesh.mesh_terrains_cfg.MeshFloatingRingTerrainCfg(
             proportion=0.3,                           # 完全生成此地形
             ring_width_range=(2.0, 2.0),              # 环的宽度范围（中心向外延伸 0.5~1.0 米）
-            ring_height_range=(0.1, 0.5),             # 环的离地高度范围
-            ring_thickness=0.01,                       # 环厚度（z 方向）
+            ring_height_range=(0.0, 0.4),             # 环的离地高度范围
+            ring_thickness=0.1,                       # 环厚度（z 方向）
             platform_width=3.0,                       # 地形中心的方形平台大小
             size=(5.0, 5.0),                        # 每块地形大小
             flat_patch_sampling={
