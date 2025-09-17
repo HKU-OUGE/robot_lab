a4c8320d89666bf0ff9861edb51919066563b45e
唐天扬 <ttyaigsw@gmail.com>
2025-09-17 20:52:59 +0800

    feat: SiriusW usd update & command update

--- source/robot_lab/robot_lab/tasks/locomotion/velocity/velocity_env_cfg.py
+++ source/robot_lab/robot_lab/tasks/locomotion/velocity/velocity_env_cfg.py
@@ -126,7 +126,7 @@ class MySceneCfg(InteractiveSceneCfg):
 class CommandsCfg:
     """Command specifications for the MDP."""
 
-    base_velocity = mdp.UniformVelocityCommandCfg(
+    base_velocity = mdp.UniformThresholdVelocityCommand(
         asset_name="robot",
         resampling_time_range=(10.0, 10.0),
         rel_standing_envs=0.02,
@@ -134,7 +134,7 @@ class CommandsCfg:
         heading_command=True,
         heading_control_stiffness=0.5,
         debug_vis=True,
-        ranges=mdp.UniformVelocityCommandCfg.Ranges(
+        ranges=mdp.UniformThresholdVelocityCommand.Ranges(
             lin_vel_x=(-1.0, 1.0), lin_vel_y=(-1.0, 1.0), ang_vel_z=(-1.0, 1.0), heading=(-math.pi, math.pi)
             # lin_vel_x=(0.0, 1.0), lin_vel_y=(0.0, 0.0), ang_vel_z=(0.0, 0.0), heading=(-math.pi, math.pi)
         ),
