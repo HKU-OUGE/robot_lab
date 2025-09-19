589dbaea9e8c24eb8db7ad8f260dc689465c79a0
TANG Tianyang <ttyaigsw@gmail.com>
2025-09-19 20:08:53 +0800

    test:2025-09-19_19-30-42

--- source/robot_lab/robot_lab/tasks/locomotion/velocity/config/wheeled/sirius_wheel/ring_env_cfg.py
+++ source/robot_lab/robot_lab/tasks/locomotion/velocity/config/wheeled/sirius_wheel/ring_env_cfg.py
@@ -413,11 +413,11 @@ class CUHKLRLSiriusWRingEnvCfg(LocomotionVelocityRoughEnvCfg):
         # ------------------------------Terminations------------------------------
         # self.terminations.illegal_contact.params["sensor_cfg"].body_names = [self.base_link_name, ".*_hip"]
         self.terminations.illegal_contact.params["sensor_cfg"].body_names = [self.base_link_name]
-        # self.terminations.illegal_contact = None
+        self.terminations.illegal_contact = None
         # ------------------------------Commands------------------------------
         # ------------------------------Commands------------------------------
         self.commands.base_velocity.ranges.lin_vel_x = (0.0, 0.6)
         self.commands.base_velocity.ranges.lin_vel_y = (0.0, 0.0)
-        self.commands.base_velocity.ranges.ang_vel_z = (-0.5, 0.5)
+        self.commands.base_velocity.ranges.ang_vel_z = (-0.3, 0.3)
         self.commands.base_velocity.ranges.heading = (3.14,3.14)
         self.curriculum.command_levels.params["range_multiplier"] = (1.0, 1.0)
--- source/robot_lab/robot_lab/terrains/config/rough.py
+++ source/robot_lab/robot_lab/terrains/config/rough.py
@@ -181,7 +181,7 @@ PLANE_TERRAINS_CFG = TerrainGeneratorCfg(
 
 
 FLOATING_RING_TERRAINS_CFG = TerrainGeneratorCfg(
-    size=(12.0, 12.0),       # 整个 terrain tile 尺寸
+    size=(13.0, 13.0),       # 整个 terrain tile 尺寸
     border_width=20.0,        # 地形边界，防止掉落
     num_rows=12,
     num_cols=20,
@@ -195,8 +195,8 @@ FLOATING_RING_TERRAINS_CFG = TerrainGeneratorCfg(
             ring_width_range=(2.0, 3.0),              # 环的宽度范围（中心向外延伸 0.5~1.0 米）
             ring_height_range=(-0.25, 0.5),             # 环的离地高度范围
             ring_thickness=0.25,                       # 环厚度（z 方向）
-            platform_width=2.0,                       # 地形中心的方形平台大小
-            size=(10.0, 10.0),                        # 每块地形大小
+            platform_width=3.0,                       # 地形中心的方形平台大小
+            size=(11.0, 11.0),                        # 每块地形大小
             flat_patch_sampling={
                 "target": FlatPatchSamplingCfg(
                     num_patches=50,     
@@ -209,7 +209,7 @@ FLOATING_RING_TERRAINS_CFG = TerrainGeneratorCfg(
             proportion=0.15,
             step_height_range=(0.00, 0.75),
             step_width=2.0,
-            platform_width=2.0,
+            platform_width=3.0,
             border_width=2.0,
             holes=False,
             flat_patch_sampling={
@@ -224,7 +224,7 @@ FLOATING_RING_TERRAINS_CFG = TerrainGeneratorCfg(
             proportion=0.15,
             step_height_range=(0.00, 0.75),
             step_width=2.0,
-            platform_width=2.0,
+            platform_width=3.0,
             border_width=2.0,
             holes=False,
             flat_patch_sampling={
@@ -236,7 +236,7 @@ FLOATING_RING_TERRAINS_CFG = TerrainGeneratorCfg(
             },
         ),
         "hf_pyramid_slope": terrain_gen.HfPyramidSlopedTerrainCfg(
-            proportion=0.10, slope_range=(0.0, 0.4), platform_width=2.0, border_width=2.0,
+            proportion=0.10, slope_range=(0.0, 0.4), platform_width=3.0, border_width=2.0,
             flat_patch_sampling={
                 "target": FlatPatchSamplingCfg(
                     num_patches=50,   
@@ -246,7 +246,7 @@ FLOATING_RING_TERRAINS_CFG = TerrainGeneratorCfg(
             },
         ),
         "hf_pyramid_slope_inv": terrain_gen.HfInvertedPyramidSlopedTerrainCfg(
-            proportion=0.10, slope_range=(0.0, 0.4), platform_width=2.0, border_width=2.0,
+            proportion=0.10, slope_range=(0.0, 0.4), platform_width=3.0, border_width=2.0,
             flat_patch_sampling={
                 "target": FlatPatchSamplingCfg(
                     num_patches=50,    
