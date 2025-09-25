8d15e8c39555292f562d87a257ec35c1de78e013
唐天扬 <ttyaigsw@gmail.com>
2025-09-25 17:57:54 +0800

    fix: Update terrain & model

--- source/robot_lab/robot_lab/terrains/config/rough.py
+++ source/robot_lab/robot_lab/terrains/config/rough.py
@@ -291,7 +291,7 @@ FLOATING_CAR_TERRAINS_CFG = TerrainGeneratorCfg(
         "floating_ring": terrain_gen.trimesh.mesh_terrains_cfg.MeshFloatingRingTerrainCfg(
             proportion=1.0,                           # 完全生成此地形
             ring_width_range=(1.5, 1.5),              # 环的宽度范围（中心向外延伸 0.5~1.0 米）
-            ring_height_range=(0.15, 0.15),             # 环的离地高度范围
+            ring_height_range=(0.4, 0.25),             # 环的离地高度范围
             ring_thickness=0.05,                       # 环厚度（z 方向）
             platform_width=5.0,                       # 地形中心的方形平台大小
             size=(10.0, 10.0),                        # 每块地形大小
