317f13ba377b1ffe7a8253f2959308220b87354e
TANG Tianyang <ttyaigsw@gmail.com>
2025-09-28 22:27:44 +0800

    test:2025-09-28_22-09-49

--- source/robot_lab/robot_lab/terrains/config/rough.py
+++ source/robot_lab/robot_lab/terrains/config/rough.py
@@ -112,19 +112,19 @@ EASY_RING_TERRAINS_CFG = TerrainGeneratorCfg(
         "pyramid_stairs": terrain_gen.MeshPyramidStairsTerrainCfg(
             proportion=0.2,
             step_height_range=(0.05, 0.23),
-            step_width=0.3,
+            step_width=2.0,
             platform_width=3.0,
             border_width=1.0,
             holes=False,
         ),
-        "pyramid_stairs_inv": terrain_gen.MeshInvertedPyramidStairsTerrainCfg(
-            proportion=0.2,
-            step_height_range=(0.05, 0.23),
-            step_width=0.3,
-            platform_width=3.0,
-            border_width=1.0,
-            holes=False,
-        ),
+        # "pyramid_stairs_inv": terrain_gen.MeshInvertedPyramidStairsTerrainCfg(
+        #     proportion=0.2,
+        #     step_height_range=(0.05, 0.23),
+        #     step_width=0.3,
+        #     platform_width=3.0,
+        #     border_width=1.0,
+        #     holes=False,
+        # ),
         "boxes": terrain_gen.MeshRandomGridTerrainCfg(
             proportion=0.1, grid_width=0.45, grid_height_range=(0.05, 0.2), platform_width=2.0
         ),
@@ -132,10 +132,10 @@ EASY_RING_TERRAINS_CFG = TerrainGeneratorCfg(
             proportion=0.1, noise_range=(0.01, 0.10), noise_step=0.02, border_width=0.25
         ),
         "hf_pyramid_slope": terrain_gen.HfPyramidSlopedTerrainCfg(
-            proportion=0.1, slope_range=(0.0, 0.4), platform_width=2.0, border_width=0.25
+            proportion=0.2, slope_range=(0.0, 0.4), platform_width=2.0, border_width=0.25
         ),
         "hf_pyramid_slope_inv": terrain_gen.HfInvertedPyramidSlopedTerrainCfg(
-            proportion=0.1, slope_range=(0.0, 0.4), platform_width=2.0, border_width=0.25
+            proportion=0.2, slope_range=(0.0, 0.4), platform_width=2.0, border_width=0.25
         ),
     },
 )
