94f51e479818b4cb37882bbebd937255df538a91
Amy8722 <xinqili551@gmail.com>
2026-02-05 23:02:13 +0800

    test: 2026-02-05_17-55-51

--- source/robot_lab/robot_lab/tasks/locomotion/velocity/config/quadruped/Arcdog_adjustable_leg/rough_env_cfg.py
+++ source/robot_lab/robot_lab/tasks/locomotion/velocity/config/quadruped/Arcdog_adjustable_leg/rough_env_cfg.py
@@ -142,7 +142,7 @@ class ArclabArcdogAdjustableLegRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         self.rewards.ang_vel_xy_l2.weight = -0.2
         self.rewards.flat_orientation_l2.weight = -2.0
         self.rewards.base_height_l2.weight = -4
-        self.rewards.base_height_l2.params["target_height"] = 0.38
+        self.rewards.base_height_l2.params["target_height"] = 0.40
         self.rewards.base_height_l2.params["asset_cfg"].body_names = [
             self.base_link_name
         ]
@@ -202,7 +202,7 @@ class ArclabArcdogAdjustableLegRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         # self.rewards.joint_position_penalty.params["stand_still_scale"] = 1.5
         # self.rewards.joint_position_penalty.params["velocity_threshold"] = 0.3
         self.rewards.rotate_joint_pos_penalty.weight = -0.6
-        self.rewards.prismatic_joint_pos_penalty.weight = -15
+        self.rewards.prismatic_joint_pos_penalty.weight = -5
         self.rewards.feet_height_exp.weight = 2.0
         self.rewards.feet_height_exp.params["target_height"] = 0.12
         self.rewards.feet_height_exp.params["asset_cfg"].body_names = [
@@ -213,7 +213,7 @@ class ArclabArcdogAdjustableLegRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         # self.rewards.feet_height_body_exp.params["asset_cfg"].body_names = [
         #     self.foot_link_name
         # ]
-        self.rewards.feet_gait.weight = 2.0
+        self.rewards.feet_gait.weight = 4.0
         self.rewards.feet_gait.params["velocity_threshold"] = 0.5
         # trotting
         self.rewards.feet_gait.params["synced_feet_pair_names"] = (
