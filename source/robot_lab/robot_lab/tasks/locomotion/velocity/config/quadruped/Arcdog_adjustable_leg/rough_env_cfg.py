aaa53ddfc66337a65989ee734e668f32f8ae1d1f
Amy8722 <xinqili551@gmail.com>
2026-02-09 02:08:48 +0800

    test: 2026-02-09_01-24-25(short leg)

--- source/robot_lab/robot_lab/tasks/locomotion/velocity/config/quadruped/Arcdog_adjustable_leg/rough_env_cfg.py
+++ source/robot_lab/robot_lab/tasks/locomotion/velocity/config/quadruped/Arcdog_adjustable_leg/rough_env_cfg.py
@@ -143,7 +143,7 @@ class ArclabArcdogAdjustableLegRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         self.rewards.ang_vel_xy_l2.weight = -0.2
         self.rewards.flat_orientation_l2.weight = -2.0
         self.rewards.base_height_l2.weight = -4
-        self.rewards.base_height_l2.params["target_height"] = 0.45
+        self.rewards.base_height_l2.params["target_height"] = 0.40
         self.rewards.base_height_l2.params["asset_cfg"].body_names = [
             self.base_link_name
         ]
