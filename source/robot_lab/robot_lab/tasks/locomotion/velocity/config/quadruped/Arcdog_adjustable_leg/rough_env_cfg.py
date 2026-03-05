4f2256bf5528d42466d035c29735515ef250736f
Amy8722 <xinqili551@gmail.com>
2026-03-05 23:20:23 +0800

    test: 2026-03-05_20-33-14

--- source/robot_lab/robot_lab/tasks/locomotion/velocity/config/quadruped/Arcdog_adjustable_leg/rough_env_cfg.py
+++ source/robot_lab/robot_lab/tasks/locomotion/velocity/config/quadruped/Arcdog_adjustable_leg/rough_env_cfg.py
@@ -50,7 +50,7 @@ class ArcdogAdjustableLegRewardsCfg(RewardsCfg):
         weight=0.0,  # 默认权重设为0，在 EnvCfg 中具体配置
         params={
             "command_name": "base_velocity",
-            "std": 0.2,               # 控制对倾斜的敏感度，越小越严格
+            "std": 0.1,               # 控制对倾斜的敏感度，越小越严格
             "command_threshold": 0.1,  # 速度指令小于此值视为静止
             "asset_cfg": SceneEntityCfg("robot"),
         },
@@ -238,7 +238,7 @@ class ArclabArcdogAdjustableLegRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         # self.rewards.joint_position_penalty.weight = -0.9
         # self.rewards.joint_position_penalty.params["stand_still_scale"] = 1.5
         # self.rewards.joint_position_penalty.params["velocity_threshold"] = 0.3
-        self.rewards.rotate_joint_pos_penalty.weight = -0.05
+        self.rewards.rotate_joint_pos_penalty.weight = -0.01
         self.rewards.prismatic_joint_pos_penalty.weight = -3
         self.rewards.feet_height_exp.weight = 2.0
         self.rewards.feet_height_exp.params["target_height"] = 0.18
